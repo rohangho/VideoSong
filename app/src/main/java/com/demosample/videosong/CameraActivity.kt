b95f51dea4bffb3fbb79dca894315379e4e57beb
Rohan Ghosh <rohan@khoslalabs.com>
2020-08-27 14:18:26 +0530

    Added camrea xml

--- app/src/main/java/com/demosample/videosong/CameraActivity.kt
+++ app/src/main/java/com/demosample/videosong/CameraActivity.kt
@@ -1,14 +1,59 @@
-package com.demosample.videosong;
+package com.demosample.videosong
 
-import android.os.Bundle;
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
-import androidx.appcompat.app.AppCompatActivity;
+class CameraActivity : AppCompatActivity() {
+    var a = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+        a = intent.getIntExtra("song", 0)
 
-public class CameraActivity extends AppCompatActivity {
+        checkGivenPermission()
+    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
+    private fun checkGivenPermission() {
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ===
+                        PackageManager.PERMISSION_GRANTED)
+        ) {
+
+
+        } else {
+            if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                Toast.makeText(
+                        this,
+                        "Please Give All Permission", Toast.LENGTH_SHORT
+                ).show()
+            }
+            requestPermissions(
+                    arrayOf(
+                            Manifest.permission.CAMERA,
+
+                            ),
+                    1001
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1001) {
+            Toast.makeText(
+                    this,
+                    "Permission Given", Toast.LENGTH_SHORT
+            ).show()
+
+            openCamera()
+
+        }
+    }
+
+    private fun openCamera() {
+
     }
 }
--- app/src/main/java/com/demosample/videosong/MainActivity.java
+++ app/src/main/java/com/demosample/videosong/MainActivity.java
@@ -1,31 +1,55 @@
 package com.demosample.videosong;
 
-import android.content.res.AssetFileDescriptor;
-import android.media.MediaPlayer;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.io.IOException;
+import androidx.appcompat.widget.AppCompatTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    AppCompatTextView songOne;
+    AppCompatTextView song2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MediaPlayer mediaPlayer = new MediaPlayer();
-        AssetFileDescriptor afd;
-        try {
-            afd = getAssets().openFd("a.mp3");
-            mediaPlayer.setDataSource(afd.getFileDescriptor());
-            mediaPlayer.prepare();
-            mediaPlayer.start();
-        } catch (IOException e) {
-            e.printStackTrace();
+        songOne = findViewById(R.id.song1);
+        song2 = findViewById(R.id.song2);
+        songOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startCamera(1);
+            }
+        });
+        song2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startCamera(2);
+            }
+        });
+//        MediaPlayer mediaPlayer = new MediaPlayer();
+//        AssetFileDescriptor afd;
+//        try {
+//            afd = getAssets().openFd("a.mp3");
+//            mediaPlayer.setDataSource(afd.getFileDescriptor());
+//            mediaPlayer.prepare();
+//            mediaPlayer.start();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        Toast.makeText(MainActivity.this, "Playing" + "a",
+//                Toast.LENGTH_SHORT).show();
+    }
+
+    private void startCamera(int a) {
+        if (a == 1)
+            startActivity(new Intent(this, CameraActivity.class).putExtra("song", 1));
+        else {
+            startActivity(new Intent(this, CameraActivity.class).putExtra("song", 2));
         }
-        Toast.makeText(MainActivity.this, "Playing" + "a",
-                Toast.LENGTH_SHORT).show();
     }
 }
