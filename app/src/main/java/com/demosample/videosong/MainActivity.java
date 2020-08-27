0e1e5749998970f35bacd6d3bdb99046b6af37fe
Rohan Ghosh <rohangho@live.com>
2020-08-27 11:59:38 +0530

    Added the camera activity

--- app/src/main/java/com/demosample/videosong/MainActivity.java
+++ app/src/main/java/com/demosample/videosong/MainActivity.java
@@ -1,8 +1,13 @@
 package com.demosample.videosong;
 
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        AssetFileDescriptor afd;
+        try {
+            afd = getAssets().openFd("a.mp3");
+            mediaPlayer.setDataSource(afd.getFileDescriptor());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Toast.makeText(MainActivity.this, "Playing" + "a",
+                Toast.LENGTH_SHORT).show();
     }
 }
